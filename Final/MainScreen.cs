82a71f6efd6203d60e3ea6553404e6d529611cd4
vanchinh12042002 <truongchinh708@gmail.com>
2022-12-26 14:47:24 +0700

    thay doi ten

--- Final/MainScreen.cs
+++ Final/MainScreen.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Final
 {
-    public partial class Form1 : Form
+    public partial class MainScreen : Form
     {
-        public Form1()
+        public MainScreen()
         {
             InitializeComponent();
         }
